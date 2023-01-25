b36b61b84140b9f42e3e954c2ec0387529d25d48
Samppa Alatalo <samppa.alatalo@gmail.com>
2023-01-25 22:54:55 +0200

    Doc edit

--- LinearSearch.h
+++ LinearSearch.h
@@ -14,7 +14,7 @@ namespace Algorithms
          * @brief   Find the index of the requested value in a sorted vector of integers input.
          *          Sorting to be added after getting to sorting algorithms.
          * 
-         * @pre     input is of type int and sorted in a non-decreasing order
+         * @pre     input content is of type int and sorted in a non-decreasing order
          * @post    return is of type int signifying the index of value or -1 if not found
          *          && input is unchanged
          * @return  int: Index of value in input or -1 if not found
@@ -23,7 +23,7 @@ namespace Algorithms
         /**
          * @brief   Find the index of the requested value in an unsorted vector of integers input.
          * 
-         * @pre     input is of type int
+         * @pre     input content is of type int
          * @post    return is of type int signifying the index of value or -1 if not found
          *          && input is unchanged
          * @return  int: Index of value in input vector or -1 if not found
