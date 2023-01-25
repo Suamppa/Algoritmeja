06a177ffcf748a63b19d4db5f67063fca5eb46e5
Samppa Alatalo <samppa.alatalo@gmail.com>
2023-01-25 14:34:10 +0200

    Finished contracts, named parameters in header and cleaned up leftover comments

--- LinearSearch.h
+++ LinearSearch.h
@@ -7,19 +7,27 @@ namespace Algorithms
 {
     class LinearSearch
     {
-    // private:
-    //     LinearSearch();
     public:
         // How to generalize (don't force container type)?
 
         /**
-         * @brief Find the index of the requested value in a sorted vector of integers.
+         * @brief   Find the index of the requested value in a sorted vector of integers input.
          * 
-         * @pre Input vector is of type int and sorted
-         * @return Index of value or -1 for error
+         * @pre     input is of type int and sorted in a non-decreasing order
+         * @post    return is of type int signifying the index of value or -1 if not found
+         *          && input is unchanged
+         * @return  int: Index of value in input or -1 if not found
          */
-        static int sortedLinearSearch(int, std::vector<int> &);
-        static int unsortedLinearSearch(int, std::vector<int> &);
+        static int sortedLinearSearch(int value, std::vector<int> & input);
+        /**
+         * @brief   Find the index of the requested value in an unsorted vector of integers input.
+         * 
+         * @pre     input is of type int
+         * @post    return is of type int signifying the index of value or -1 if not found
+         *          && input is unchanged
+         * @return  int: Index of value in input vector or -1 if not found
+         */
+        static int unsortedLinearSearch(int value, std::vector<int> & input);
     };
 }
 
