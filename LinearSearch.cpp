9e85a65a7c493b961c0a097cdb116c090eeed050
Samppa Alatalo <samppa.alatalo@gmail.com>
2023-01-26 20:54:08 +0200

    Generalization, testing and documenting

--- LinearSearch.cpp
+++ LinearSearch.cpp
@@ -2,19 +2,45 @@
 
 namespace Algorithms
 {
-    int LinearSearch::sortedLinearSearch(int value, std::vector<int> & input) {
+    template<typename T, class Container>
+    int LinearSearch::sortedLinearSearch(T value, Container & input) {
         int inputLength = input.size();
         for (int i = 0; i < inputLength; i++) {
-            int x = input.at(i);
+            T x = input.at(i);
             if (x == value) return i;
             if (x > value) return -1;   // Not found
         }
+        return -1;  // Not found
     }
 
-    int LinearSearch::unsortedLinearSearch(int value, std::vector<int> & input) {
+    template<typename T, typename InputIterator>
+    int LinearSearch::sortedLinearSearch(T value, InputIterator first, InputIterator last) {
+        int index = 0;
+        while (first != last) {
+            if (*first == value) return index;
+            if (*first > value) return -1;  // Not found
+            ++index;
+            ++first;
+        }
+        return -1;  // Not found
+    }
+
+    template<typename T, class Container>
+    int LinearSearch::unsortedLinearSearch(T value, Container & input) {
         int inputLength = input.size();
         for (int i = 0; i < inputLength; i++) {
             if (input.at(i) == value) return i;
+        }
+        return -1;  // Not found
+    }
+
+    template<typename T, typename InputIterator>
+    int LinearSearch::unsortedLinearSearch(T value, InputIterator first, InputIterator last) {
+        int index = 0;
+        while (first != last) {
+            if (*first == value) return index;
+            ++index;
+            ++first;
         }
         return -1;  // Not found
     }
--- LinearSearch.h
+++ LinearSearch.h
@@ -1,34 +1,79 @@
 #ifndef LINEARSEARCH_H
 #define LINEARSEARCH_H
 
-#include <vector>
+// #include "Algorithms.h"
+// #include <vector>
 
 namespace Algorithms
 {
     class LinearSearch
     {
     public:
-        // How to generalize (don't force container type)?
-
         /**
-         * @brief   Find the index of the requested value in a sorted vector of integers input.
+         * @brief   Find the index of the requested value in a sorted container input.
          *          Sorting to be added after getting to sorting algorithms.
          * 
-         * @pre     input content is of type int and sorted in a non-decreasing order
+         * @tparam  T the type of data stored in the container (or convertible)
+         * @tparam  Container the container type, must provide functions `size()` and `at()`
+         * @param   value the value to search for
+         * @param   input the container to be searched
+         * @pre     input content is of a comparable type convertible to T and sorted in a non-decreasing order
+         *          && container class provides functions `size()` and `at()`
          * @post    return is of type int signifying the index of value or -1 if not found
          *          && input is unchanged
-         * @return  int: Index of value in input or -1 if not found
+         * @returns int, index of value in input or -1 if not found
          */
-        static int sortedLinearSearch(int value, std::vector<int> & input);
+        template<typename T, class Container>
+        static int sortedLinearSearch(T value, Container & input);
+
         /**
-         * @brief   Find the index of the requested value in an unsorted vector of integers input.
+         * @brief   Find the index of the requested value within a sorted container.
+         *          Sorting to be added after getting to sorting algorithms.
          * 
-         * @pre     input content is of type int
+         * @tparam  T the type of data stored in the container (or convertible)
+         * @tparam  InputIterator input iterator type
+         * @param   value the value to search for
+         * @param   first iterator pointing to where the search starts
+         * @param   last iterator pointing to where the search ends
+         * @pre     input content is of a comparable type convertible to T and sorted in a non-decreasing order
          * @post    return is of type int signifying the index of value or -1 if not found
          *          && input is unchanged
-         * @return  int: Index of value in input vector or -1 if not found
+         * @returns int, index of value in the container or -1 if not found
          */
-        static int unsortedLinearSearch(int value, std::vector<int> & input);
+        template<typename T, typename InputIterator>
+        static int sortedLinearSearch(T value, InputIterator first, InputIterator last);
+
+        /**
+         * @brief   Find the index of the requested value in an unsorted container input.
+         * 
+         * @tparam  T the type of data stored in the container (or convertible)
+         * @tparam  Container the container type, must provide functions `size()` and `at()`
+         * @param   value the value to search for
+         * @param   input the container to be searched
+         * @pre     input content is of a comparable type convertible to T
+         *          && container class provides functions `size()` and `at()`
+         * @post    return is of type int signifying the index of value or -1 if not found
+         *          && input is unchanged
+         * @returns int, index of value in input or -1 if not found
+         */
+        template<typename T, class Container>
+        static int unsortedLinearSearch(T value, Container & input);
+
+        /**
+         * @brief   Find the index of the requested value in an unsorted container input.
+         * 
+         * @tparam  T the type of data stored in the container (or convertible)
+         * @tparam  InputIterator input iterator type
+         * @param   value the value to search for
+         * @param   first iterator pointing to where the search starts
+         * @param   last iterator pointing to where the search ends
+         * @pre     first and last are not null
+         * @post    return is of type int signifying the index of value or -1 if not found
+         *          && input is unchanged
+         * @returns int, index of value in input or -1 if not found
+         */
+        template<typename T, typename InputIterator>
+        static int unsortedLinearSearch(T value, InputIterator first, InputIterator last);
     };
 }
 
