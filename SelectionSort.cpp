ea9a5075cff01917d69f808b64f722812beb618d
Samppa Alatalo <samppa.alatalo@gmail.com>
2023-02-01 14:40:04 +0200

    Use shared Algorithms::swap function

--- SelectionSort.cpp
+++ SelectionSort.cpp
@@ -10,9 +10,7 @@ namespace Algorithms
             for (int j = i + 1; j < size; ++j) {
                 if (container.at(j) < container.at(minind)) minind = j;
             }
-            int temp = container.at(i);
-            container.at(i) = container.at(minind);
-            container.at(minind) = temp;
+            swap(container, i, minind);
         }
     }
 }
