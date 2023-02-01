44305c59d37353e0588945bf5f6e674ac51fe16c
Samppa Alatalo <samppa.alatalo@gmail.com>
2023-02-01 14:39:24 +0200

    Add file for common namespace utilities

---
+++ Algorithms.h
@@ -0,0 +1,11 @@
+/** Utilities for the Algorithms namespace */
+
+#ifndef ALGORITHMS_H
+#define ALGORITHMS_H
+
+namespace Algorithms {
+    template<class Container>
+    void swap(Container & container, int index1, int index2);
+}
+
+#endif // !ALGORITHMS_H
