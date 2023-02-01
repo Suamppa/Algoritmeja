99b4877596c795c5970c013184ad90562fa6fac8
Samppa Alatalo <samppa.alatalo@gmail.com>
2023-02-01 15:12:04 +0200

    Added documentation blocks

--- Algorithms.h
+++ Algorithms.h
@@ -3,6 +3,9 @@
 #ifndef ALGORITHMS_H
 #define ALGORITHMS_H
 
+/**
+ * Shared namespace for all the included algorithms
+ */
 namespace Algorithms {
     template<class Container>
     void swap(Container & container, int index1, int index2);
--- SelectionSort.h
+++ SelectionSort.h
@@ -5,6 +5,15 @@
 
 namespace Algorithms
 {
+    /**
+     * @brief   Sort the container in non-descending order using a selection sort algorithm.
+     *          The algorithm is not stable.
+     * 
+     * @tparam  Container the container type, must provide functions `size()` and `at()`
+     * @param   container container to sort, will be modified directly
+     * @pre     container data is modifiable && container data is comparable
+     * @post    container data is sorted in a non-descending order
+     */
     template<class Container>
     void selectionSort(Container & container);
 }
