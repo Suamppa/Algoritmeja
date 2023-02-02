043ce2150f6324d4730e63f7e5e2aacc11946a78
Samppa Alatalo <samppa.alatalo@gmail.com>
2023-02-02 19:22:15 +0200

    Add merge sort

---
+++ MergeSort.cpp
@@ -0,0 +1,45 @@
+#include "MergeSort.h"
+
+using namespace Algorithms;
+
+template<class Container>
+void Algorithms::mergeSort(Container & container, int start, int end) {
+    if (start < end) {
+        int middle = (start + end) / 2;
+        mergeSort(container, start, middle);
+        mergeSort(container, middle+1, end);
+        Helper::merge(container, start, middle, end);
+    }
+}
+
+template<class Container>
+void Algorithms::Helper::merge(Container & container, int start, int split, int end) {
+    int sizeA = split - start + 1;
+    int sizeB = end - split;
+    int iA = 0, iB = 0;
+    auto x = container[start];  // Sample container data type
+    typedef decltype(x) T;
+    T A[sizeA+1];
+    T B[sizeB+1];
+    for (int j = start; j <= split; ++j) {
+        A[iA] = container[j];
+        ++iA;
+    }
+    for (int j = split+1; j <= end; ++j) {
+        B[iB] = container[j];
+        ++iB;
+    }
+    A[sizeA] = intMax();
+    B[sizeB] = intMax();
+    iA = 0;
+    iB = 0;
+    for (int j = start; j <= end; ++j) {
+        if (iA < sizeA && A[iA] <= B[iB]) {
+            container[j] = A[iA];
+            ++iA;
+        } else if (iB < sizeB) {
+            container[j] = B[iB];
+            ++iB;
+        }
+    }
+}
