e6404b5ac46b386d6f5236ca19ea6bb76d01640b
Samppa Alatalo <samppa.alatalo@gmail.com>
2023-02-03 15:08:04 +0200

    Removed now obsolete intMax

--- Algorithms.cpp
+++ Algorithms.cpp
@@ -8,10 +8,3 @@ void Algorithms::swap(Container & container, int index1, int index2) {
     container.at(index1) = container.at(index2);
     container.at(index2) = temp;
 }
-
-int Algorithms::intMax() {
-    int i = 0;
-    i = ~i;
-    i = (unsigned int) i >> 1;
-    return i;
-}
