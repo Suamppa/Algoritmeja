01772b3baee3171d45626ec8b497eecca01cbf0b
Samppa Alatalo <samppa.alatalo@gmail.com>
2023-02-09 16:36:37 +0200

    Add Heap class in preparation for heapsort

--- Algorithms.cpp
+++ Algorithms.cpp
@@ -8,3 +8,72 @@ void Algorithms::swap(Container & container, int index1, int index2) {
     container.at(index1) = container.at(index2);
     container.at(index2) = temp;
 }
+
+template<class Container>
+Algorithms::Heap<Container>::Heap(Container & container, int first, int last) :
+    container(container), start(first), end(last)
+{
+    length = end - start + 1;
+    inHeap = length;
+}
+
+template<class Container>
+int Algorithms::Heap<Container>::size() { return length; }
+
+// template<class Container>
+// int Algorithms::Heap<Container>::inHeap() { return inHeap; }
+
+template<class Container>
+int Algorithms::Heap<Container>::parent(int i) {
+    if (i <= 0) return 0;
+    return (i - 1) / 2;
+}
+
+template<class Container>
+int Algorithms::Heap<Container>::left(int i) { return 2 * i + 1; }
+
+template<class Container>
+int Algorithms::Heap<Container>::right(int i) { return 2 * i + 2; }
+
+template<class Container>
+bool Algorithms::Heap<Container>::isInHeap(int i) {
+    if (i >= start && i <= end) return true;
+    return false;
+}
+
+template<class Container>
+void Algorithms::Heap<Container>::buildMaxHeap() {
+    inHeap = length;
+    for (int i = start + (length-1)/2; i >= start; --i) fixMaxHeap(i);
+}
+
+template<class Container>
+void Algorithms::Heap<Container>::fixMaxHeap(int i) {
+    int left = this->left(i);
+    int right = this->right(i);
+    int max = i;
+    if (isInHeap(left) && container[left] > container[max]) max = left;
+    if (isInHeap(right) && container[right] > container[max]) max = right;
+    if (max != i) {
+        swap(container, i, max);
+        fixMaxHeap(max);
+    }
+}
+
+template<class Container>
+Container & Algorithms::Heap<Container>::getContainer() { return container; }
+
+template<class Container>
+auto & Algorithms::Heap<Container>::operator[](int i) { return container[i]; }
+
+/*
+template<class Container>
+void Algorithms::buildMaxHeap(Container & container) {
+
+}
+
+template<class Container>
+void Algorithms::fixMaxHeap(Container & container) {
+
+}
+ */
--- Algorithms.h
+++ Algorithms.h
@@ -17,6 +17,38 @@ namespace Algorithms {
      */
     template<class Container>
     void swap(Container & container, int index1, int index2);
+
+    /**
+     * @brief   Class to assign a container's index range to a heap structure.
+     * 
+     * @tparam  Container container type
+     */
+    template<class Container>
+    class Heap
+    {
+        Container & container;
+        int start, end, length, inHeap;
+    public:
+        Heap(Container & container, int first, int last);
+        int size();
+        int parent(int i);
+        int left(int i);
+        int right(int i);
+        bool isInHeap(int i);
+        void buildMaxHeap();
+        void fixMaxHeap(int i);
+        Container & getContainer();
+
+        auto & operator[](int i);
+    };
+
+    /*
+    template<class Container>
+    void buildMaxHeap(Container & container);
+
+    template<class Container>
+    void fixMaxHeap(Container & container);
+     */
 }
 
 #endif // !ALGORITHMS_H
