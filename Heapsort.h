aa5092a958ac2d4ae5c9519f0a0eb0605689c4c8
Samppa Alatalo <samppa.alatalo@gmail.com>
2023-02-09 17:08:02 +0200

    Add Heapsort.h

---
+++ Heapsort.h
@@ -0,0 +1,16 @@
+#ifndef HEAPSORT_H
+#define HEAPSORT_H
+
+namespace Algorithms
+{
+    /**
+     * @brief   Sort data in a heap structure
+     * 
+     * @tparam  Container heap structure's underlying container type
+     * @param   heap data to sort
+     */
+    template<class Container>
+    void heapsort(Heap<Container> & heap);
+}
+
+#endif // !HEAPSORT_H
