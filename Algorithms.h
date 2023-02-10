c2ecd5d4fe189d452f7b6199a6765aeea0ca1ef5
Samppa Alatalo <samppa.alatalo@gmail.com>
2023-02-10 15:45:20 +0200

    Add declarations

--- Algorithms.h
+++ Algorithms.h
@@ -106,7 +106,10 @@ namespace Algorithms {
         auto & operator[](int i);
     };
 
-    inline namespace Helper {}
+    inline namespace Helper {
+        template<class Container>
+        int partition(Container & container, int start, int end);
+    }
 }
 
 #endif // !ALGORITHMS_H
---
+++ Quicksort.h
@@ -0,0 +1,12 @@
+#ifndef QUICKSORT_H
+#define QUICKSORT_H
+
+#include "Algorithms.h"
+
+namespace Algorithms
+{
+    template<class Container>
+    void quicksort(Container & container, int start, int end);
+}
+
+#endif // !QUICKSORT_H
