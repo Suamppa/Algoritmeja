334c5f81ec8dd565052ae6b9dd9fdb0ad58bd7f6
Samppa Alatalo <samppa.alatalo@gmail.com>
2023-02-10 15:01:44 +0200

    Add contract to heapsort

--- Heapsort.h
+++ Heapsort.h
@@ -10,6 +10,10 @@ namespace Algorithms
      * 
      * @tparam  Container heap structure's underlying container type
      * @param   heap data to sort
+     * @pre     heap data is modifiable && heap data is comparable
+     *          && container is wrapped in a Heap class
+     * @post    container data in heap is sorted in a non-descending order
+     *          && heap is a max heap
      */
     template<class Container>
     void heapsort(Heap<Container> & heap);
