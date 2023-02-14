3c9c3943b3d334efaa4d1fbb5bc7a85f4c195d55
Samppa Alatalo <samppa.alatalo@gmail.com>
2023-02-14 12:40:01 +0200

    Add initial implementation

--- Quicksort.cpp
+++ Quicksort.cpp
@@ -4,5 +4,9 @@ using namespace Algorithms;
 
 template<class Container>
 void Algorithms::quicksort(Container & container, int start, int end) {
-    
+    if (start < end) {
+        int cut = partition(container, start, end);
+        quicksort(container, start, cut-1);
+        quicksort(container, cut+1, end);
+    }
 }
