a88a42e763168f732f80f2cfb5873e227d8225a2
Samppa Alatalo <samppa.alatalo@gmail.com>
2023-02-18 15:48:33 +0200

    Docs

--- Algorithms.h
+++ Algorithms.h
@@ -14,6 +14,8 @@ namespace Algorithms {
      * @param   container container to access, will be modified directly
      * @param   index1 the index of the element to swap with
      * @param   index2 the index of the other element to swap with
+     * @pre     container data is modifiable
+     * @post    data at indices index1 and index2 is swapped
      */
     template<class Container>
     void swap(Container & container, int index1, int index2);
@@ -29,6 +31,11 @@ namespace Algorithms {
      * @param   end end of the index range to partition
      * @param   pivot optional; the index to use as pivot, defaults to end
      * @returns int, first index of the right partition
+     * @pre     container data is modifiable && container data is comparable
+     * @post    values before the value at pivot (default = end) are less than
+     *          or equal to the value at pivot
+     *          && values after the value at pivot are greater than the value
+     *          at pivot
      */
     template<class Container>
     int partition(Container & container, int start, int end, int pivot=-1);
