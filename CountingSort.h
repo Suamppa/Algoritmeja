87cef18ce6cd7b81468ef50c7cac23976e3c97af
Samppa Alatalo <samppa.alatalo@gmail.com>
2023-02-20 12:49:38 +0200

    Add CountingSort.h

---
+++ CountingSort.h
@@ -0,0 +1,12 @@
+#ifndef COUNTINGSORT_H
+#define COUNTINGSORT_H
+
+#include "Algorithms.h"
+
+namespace Algorithms
+{
+    template<class Container>
+    void countingSort(Container & container, Container & result, int maxValue);
+}
+
+#endif // !COUNTINGSORT_H
