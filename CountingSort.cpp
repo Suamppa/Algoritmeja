737a0e47d5422430762ebdac147ade1703ef25d8
Samppa Alatalo <samppa.alatalo@gmail.com>
2023-02-20 12:50:03 +0200

    Add CountingSort.cpp

---
+++ CountingSort.cpp
@@ -0,0 +1,8 @@
+#include "CountingSort.h"
+
+using namespace Algorithms;
+
+template<class Container>
+void Algorithms::countingSort(Container & container, Container & result, int maxValue) {
+    int freq[maxValue] = {};    // Frequency vector initialized with zeroes
+}
