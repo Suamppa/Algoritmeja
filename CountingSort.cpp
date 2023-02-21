a9f1ec2434d73f2fb284c71fa787c933f51a919c
Samppa Alatalo <samppa.alatalo@gmail.com>
2023-02-21 16:54:11 +0200

    Removed test prints after remembering that debuggers are a thing...

--- CountingSort.cpp
+++ CountingSort.cpp
@@ -5,12 +5,6 @@ using namespace Algorithms;
 template<class Container>
 void Algorithms::countingSort(Container & container, Container & result, int maxValue) {
     int freq[maxValue+1] = {}; // Frequency vector initialized with zeroes
-    
-    // Test print
-    std::cout << "[";
-    for (int x : freq) std::cout << x << ", ";
-    std::cout << "]" << std::endl;
-
     for (int x : container) ++freq[x]; // Increment the frequencies of values
     for (int i = 1; i <= maxValue; ++i) freq[i] += freq[i-1]; // Cumulate
     // Fill result
