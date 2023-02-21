41889a6c352eb0a53180c5c7765a9697179f2947
Samppa Alatalo <samppa.alatalo@gmail.com>
2023-02-21 18:12:06 +0200

    Finish implementation

--- CountingSort.cpp
+++ CountingSort.cpp
@@ -4,8 +4,22 @@ using namespace Algorithms;
 
 template<class Container>
 void Algorithms::countingSort(Container & container, Container & result, int maxValue) {
-    int freq[maxValue+1] = {}; // Frequency vector initialized with zeroes
-    for (int x : container) ++freq[x]; // Increment the frequencies of values
-    for (int i = 1; i <= maxValue; ++i) freq[i] += freq[i-1]; // Cumulate
-    // Fill result
+    int size = 0;
+    int freq[maxValue+1] = {}; // Frequency array initialized with zeros
+
+    for (int x : container) {
+        ++freq[x];  // Increment the frequencies of each element x in container
+        ++size;     // Also determine the size of container in the process
+    }
+
+    // Make frequencies cumulative,
+    // i.e. each element freq[i] tells how many elements in container are <= i
+    for (int i = 1; i <= maxValue; ++i) freq[i] += freq[i-1];
+
+    // Fill result starting from the end of container
+    for (int j = size-1; j >= 0; --j) {
+        // freq[container[j]] tells how many elements in container are <= container[j]
+        result[freq[container[j]]-1] = container[j]; // Place container[j] in its correct position
+        --freq[container[j]]; // Reduce the relevant cumulative frequency by one
+    }
 }
