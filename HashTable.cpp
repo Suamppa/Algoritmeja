21401fb8f3857c55ce87eb3538a47ba3b46c100b
Samppa Alatalo <samppa.alatalo@gmail.com>
2023-02-22 18:36:35 +0200

    Add HashTable files

---
+++ HashTable.cpp
@@ -0,0 +1,5 @@
+#include "HashTable.h"
+
+using namespace Algorithms;
+
+
---
+++ HashTable.h
@@ -0,0 +1,11 @@
+#ifndef HASHTABLE_H
+#define HASHTABLE_H
+
+#include "Algorithms.h"
+
+namespace Algorithms
+{
+    
+}
+
+#endif // !HASHTABLE_H
