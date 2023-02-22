efef883ec4d17cb6f414217261ca2d9ac9668fa2
Samppa Alatalo <samppa.alatalo@gmail.com>
2023-02-22 19:10:19 +0200

    Add nested class Key and its initial declaration

--- HashTable.h
+++ HashTable.h
@@ -2,10 +2,27 @@
 #define HASHTABLE_H
 
 #include "Algorithms.h"
+#include <vector>
 
 namespace Algorithms
 {
-    
+    template<typename T>
+    class HashTable
+    {
+        class Key
+        {
+            T value;
+            Key * link;
+            bool deleted;
+        public:
+            Key();
+            Key(T data);
+            T & value();
+            Key & link();
+            bool isDeleted();
+            void erase();
+        };
+    };
 }
 
 #endif // !HASHTABLE_H
